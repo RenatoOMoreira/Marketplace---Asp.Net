a66b774dd7de6a12fb79ab611070d1e72ae4a308
Renato Moreira <renato.oliveiramoreira@outlook.com>
2022-06-08 22:27:49 -0300

    Criação de participantes

---
+++ ExpoCenter.Mvc/Mapping/ViewModelProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ExpoCenter.Mvc.Models;
+using ExpoCenterDominio.Entidades;
+
+namespace ExpoCenter.Mvc.Mapping
+{
+    public class ViewModelProfile : Profile
+
+    {
+        public ViewModelProfile()
+        {
+            CreateMap<Participante, ParticipanteViewModel>().ReverseMap();
+        }
+    }
+}
