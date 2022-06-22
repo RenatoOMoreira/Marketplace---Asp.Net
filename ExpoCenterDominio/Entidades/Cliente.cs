5d6be7ae49a683d89bcd27f3a09fc3865d24a5b2
Renato Moreira <renato.oliveiramoreira@outlook.com>
2022-06-21 21:00:19 -0300

    Update final - Swagger

---
+++ ExpoCenterDominio/Entidades/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpoCenterDominio.Entidades
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }  
+        public DateTime DataNascimento { get; set; }    
+        
+    }
+}
